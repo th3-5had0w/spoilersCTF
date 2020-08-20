4695a965b4e5c98b7d6853c4b3fe2d6803275e81
th3_5had0w <theshadow127001@gmail.com>
2020-08-20 10:32:01 +0700

    update

--- 2020/pwn/canary/canary.c
+++ 2020/pwn/canary/canary.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
-#include <>
+#include <stdlib.h>
+#include <time.h>
 
-int main(){}
+void release_canary(){
+	int passcode1;
+	int passcode2;
+	if (passcode1 == 0xd34db33f && passcode2 == 0xc4f3b4b3){
+		printf("[SYSTEM] Experiment-1337 Released.\n");
+		printf("The canary ran around you happily and give you something...\n");
+		printf("spCTF{h0l_up_wh0_r3l3453d_th3_c4n4ry_3743840c51262b1c8fb7348e9dbef5b7}\n");
+		printf("Oh, it's a flag\n");
+	}
+	else{
+		printf("[SYSTEM] Wrong passcode, locking down lab.\n");
+		printf("So you got caught...");
+	}
+}
+
+int main(){
+	char name[100];
+	char cmd[100];
+	setbuf(stdout, NULL);
+	setbuf(stdin, NULL);
+	setbuf(stderr, NULL);
+	printf("[SYSTEM] Accessing to lab control panel...");
+	printf(" Done\n");
+	printf("Username: ");
+	gets(name);
+	printf("[SYSTEM] Logged in as ");
+	printf(name);
+	printf("\nCommand: ");
+	gets(cmd);
+}
